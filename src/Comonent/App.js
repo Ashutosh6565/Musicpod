5672aa9d9405164d295a9b403b722e592579aee9
Ashutosh <kumarashutosh7302401472@gmail.com>
2025-01-15 13:41:00 +0530

    made reverse as well as the forword button for making song forword and backword

--- src/Comonent/App.js
+++ src/Comonent/App.js
@@ -13,6 +13,9 @@ import song3img from "../static/John Denver - Country Roads.jpg";
 import song4img from "../static/Post Malone - White Iverson.png";
 import song5img from "../static/Post Malone - White Iverson.png";
 
+import wallpaper1 from "../static/wallpaper1.jpg";
+import wallpaper2 from "../static/wallpaper2.jpg";
+import wallpaper3 from "../static/wallpaper3.jpg";
 
 class App extends React.Component {
   constructor() {
@@ -21,10 +24,114 @@ class App extends React.Component {
       active: 0, //active list items
       menuItems: ["Now Playong", "Mussic", "Games", "Settings"],
       musicItem: [song1, song2, song3, song4, song5],
-      songImgItemUrl: [song1img,song2img,song3img,song4img,song5img],
-      wallpaperItems: []
+      songImgItemUrl: [song1img, song2img, song3img, song4img, song5img],
+      wallpaperItems: [wallpaper1, wallpaper2, wallpaper3],
+      songItems: [
+        "Post Malone - White Iverson",
+        "Confetti  Ghost",
+        "John Denver - Country Roads",
+        "Khalid - Young Dumb Broke",
+        "Rick Astley - Never Gonna Give You Up",
+      ],
+      songIndex: 0, //current
+      lengthMenuKey: { "-1": 3, 1: 2, 4: 4, 8: 4, 3: 2, 9: 3, 10: 2 }, //llength of a particular menu
+      menuMapping: { "-1": [0, 1, 2, 3], 1: [4, 5, 6], 3: [8, 9, 10] }, //which menu can be rendered by key menu
+      currentmenu: -2,
+      navigationStack: [], //used for navigation forward and backward
+      songUrl: song1, //current song url
+      playing: false,
+      theme: "rgb(210,210,210)",
+      audio: new Audio(song1),
+      songImgUrl: song1img,
+      wheelColor: "white",
+      wallpaper: 0,
+      noty: false,
+      notifyText: "Wallpaper Changed",
     };
   }
+
+  //function for on long press of forword button tracks are seeked forward
+  seekSongForward(e) {
+    if (this.state.currentmenu === -2) {
+      return;
+    }
+    if (this.state.playing == false) {
+      return;
+    }
+    if (e.detail.interval < 250) {
+      this.state.audio.pause();
+      let songIndex = this.state.songIndex;
+      if (songIndex === this.state.songImgUrl.length - 1) {
+        songIndex = 0;
+      } else {
+        songIndex++;
+      }
+
+      const songUrl = this.state.musicItem[songIndex];
+      const songImagUrl = this.state.songImgItemUrl[songIndex];
+      this.setState(
+        {
+          songIndex: songIndex,
+          songImagUrl: songImagUrl,
+          songUrl: songUrl,
+          audio: new Audio(songUrl),
+        },
+        () => {
+          this.state.audio.play();
+        }
+      );
+    }else if(e.detail.interval>250 && e.detail.interval<10000){
+      const interval = e.detail.interval/100;
+      this.setState((prevState) => {
+        prevState.audio.currentTime += interval;
+        return prevState
+      })
+    }
+
+    //for reverse 
+    seekSongReverse(e) {
+      if (this.state.currentmenu === -2) {
+        return;
+      }
+      if (this.state.playing == false) {
+        return;
+      }
+      if (e.detail.interval < 250) {
+        this.state.audio.pause();
+        let songIndex = this.state.songIndex;
+        if (songIndex === 0) {
+          songIndex = this.state.songImgItemUrl.length-1;
+        } else {
+          songIndex--;
+        }
+  
+        const songUrl = this.state.musicItem[songIndex];
+        const songImagUrl = this.state.songImgItemUrl[songIndex];
+        this.setState(
+          {
+            songIndex: songIndex,
+            songImagUrl: songImagUrl,
+            songUrl: songUrl,
+            audio: new Audio(songUrl),
+          },
+          () => {
+            this.state.audio.play();
+          }
+        );
+      }else if(e.detail.interval>250 && e.detail.interval<10000){
+        const interval = e.detail.interval/100;
+        this.setState((prevState) => {
+          prevState.audio.currentTime -= interval;
+          return prevState
+        })
+      }
+
+  }
+
+  //toggle song play and puse
+
+
+  
   render() {
     return (
       <>
